ff73da66417dce7b3d2c15ae746666d0fc6b913c
李国帅 <oncwnuOQ65QCeGMp1VOPlkAcJ4ao@git.weixin.qq.com>
2020-04-14 21:59:00 +0800

    fix ci.yml

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'umi';
+const publicPath = './';
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  publicPath,
   locale: { antd: true },
   routes: [
     { path: '/', component: '@/pages/index' },
